621bc1878a337a76dffcf2ea15875f377a06b429
Benjamin Fleischer <github@benjaminfleischer.com>
2013-04-15 10:15:48 -0500

    Update autolink script

--- autolink.sh
+++ autolink.sh
@@ -1,1 +1,2 @@
-find . \( -iname '*.md' -o -iname '*.markdown' \) | xargs perl -pi -w -e 's/ (http[^\s]+)/ [$1]($1)/g;'
+#!/usr/bin/env bash
+find . \( -iname '*.md' -o -iname '*.markdown' \) | xargs perl -pi -w -e 's/\s(http[^\s]+)\b/ [$1]($1)/g;'
