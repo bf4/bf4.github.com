5c2484869b878032058da232d9759ef3e91329f8
Benjamin Fleischer <github@benjaminfleischer.com>
2015-06-02 21:34:53 -0500

    Test web request

--- _ember/app/components/doc-printer.js
+++ _ember/app/components/doc-printer.js
@@ -1,7 +1,13 @@
 import Ember from 'ember';
+import request from 'ic-ajax';
 
 export default Ember.Component.extend({
   init() {
     this._super(...arguments);
+    request('https://spreadsheets.google.com/feeds/list/0AqHUOZcVEj_XdE5SMzBKSWhINjVtTlh2b0JjUFp4OEE/od6/public/values?alt=json').then((data) => {
+      // do nothing with the data, for now
+      // debugger;
+      console.log(data, "Using the data so jshint is happy.");
+    });
   }
 });
