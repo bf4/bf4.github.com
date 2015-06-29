8740d999faae234aae4537d937e5d5a0a36a791d
Benjamin Fleischer <github@benjaminfleischer.com>
2015-06-29 14:06:03 -0500

    Use doc-printer with print-pair-data layout

--- _ember/app/components/doc-printer.js
+++ _ember/app/components/doc-printer.js
@@ -2,6 +2,12 @@ import Ember from 'ember';
 // import request from 'ic-ajax';
 
 export default Ember.Component.extend({
+  // 4. Make sure you specify the layoutName to match the data-template-name of your handlebars template
+  layoutName: "components/print-pair-data",
+  tagName: '',
+  config: {},
+  rows: null,
+   // hook into component initialization
   init() {
     this._super(...arguments);
     // request('https://spreadsheets.google.com/feeds/list/0AqHUOZcVEj_XdE5SMzBKSWhINjVtTlh2b0JjUFp4OEE/od6/public/values?alt=json').then((data) => {
@@ -9,5 +15,67 @@ export default Ember.Component.extend({
     //   // debugger;
     //   console.log(data, "Using the data so jshint is happy.");
     // });
-    }
+    var component = this;
+    this.getRows( function(rows) {
+      component.set('rows', rows);
+    });
+  },
+  // private
+  getRows(callback) {
+    var doc = this.GoogleSpreadsheetPrinter(this.config, Ember.$);
+    var success = function( entries ) {
+      var rows = doc.parseEntries(entries);
+      callback(rows);
+    };
+    var failure = function () {
+      // fallback to local pair data if ajax failed
+      Ember.$.getJSON('/assets/pair.json', function( json ) {
+        var entries = json.feed.entry;
+        callback( doc.parseEntries(entries) );
+      });
+    };
+    doc.fetchData(  success, failure );
+  },
+  GoogleSpreadsheetPrinter(config, helper) {
+    "use strict";
+    var doc = {};
+    doc.helper = helper;
+    doc.getConfig = (function(key) {
+      if (key) {
+        return config[key];
+      }  else {
+        return config;
+      }
+    });
+    doc.getKey = (function() { return config.key; });
+    doc.jsonURL = (function() {
+      return "https://spreadsheets.google.com/feeds/list/" +
+      doc.getKey() +
+      "/public/values?alt=json";
+    })();
+    doc.parseEntry = function(entry, fields) {
+        var row = {};
+        this.helper.each(fields, function(index, field) {
+          var field_name = "gsx$" + field;
+          var cell = entry[field_name].$t;
+          row[field] = cell;
+        });
+        return row;
+    };
+    doc.parseEntries = function(entries) {
+        var rows = [];
+        this.helper.each(entries, function(index, entry) {
+          rows.push(doc.parseEntry(entry, doc.getConfig('fields')));
+        });
+        return rows;
+    };
+    doc.fetchData = function(success, failure) {
+      this.helper.getJSON( this.jsonURL )
+        .done( function( json ) {
+          success(json.feed.entry);
+        })
+        .fail( failure );
+    };
+    return doc;
+  }
 });
