f218b67dd73ae3322a9b1fdacc07ab8933741186
Benjamin Fleischer <github@benjaminfleischer.com>
2015-09-08 10:23:16 -0500

    Update to 1.13.8

---
+++ _ember/ember-cli-build.js
@@ -0,0 +1,45 @@
+/* global require, module */
+var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+
+module.exports = function(defaults) {
+  var app = new EmberApp(defaults, {
+    storeConfigInMeta: false,
+    fingerprint: {
+      enabled: false
+    },
+    sourcemaps: {
+      "enabled": false,
+      "extensions": ["js"]
+    },
+    outputPaths: {
+      app: {
+        html: 'index.html',
+        css: { 'app': '/viewtastic.css'},
+        js: "/assets/viewtastic.js"
+      },
+      vendor: {
+        css: '/vendor.css',
+        js: "/assets/vendor.js"
+     }
+   }
+  });
+
+  // Use `app.import` to add additional libraries to the generated
+  // output files.
+  //
+  // If you need to use different assets in different
+  // environments, specify an object as the first parameter. That
+  // object's keys should be the environment name and the values
+  // should be the asset to use in that environment.
+  //
+  // If the library that you are including contains AMD or ES6
+  // modules that you would like to import into your application
+  // please specify an object with the list of modules as keys
+  // along with the exports of each module as its value.
+
+  if (EmberApp.env() !== 'production') {
+    app.import('bower_components/ember/ember-template-compiler.js');
+  }
+
+  return app.toTree();
+};
