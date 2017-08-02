6985147df71e4292070db2c5dd472aafb6833466
Benjamin Fleischer <github@benjaminfleischer.com>
2017-08-02 12:27:18 -0500

    Allow passing options to autolink

--- autolink.sh
+++ autolink.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
-find . \( -iname '*.md' -o -iname '*.markdown' \) | xargs perl -pi -w -e 's/\s(http[^\s]+)\b/ [$1]($1)/g;'
+
+find_files() {
+  find . \( -iname '*.md' -o -iname '*.markdown' \)
+}
+
+files="$@"
+if [ -z "$files" ]; then files="$(find_files)"; fi
+echo -e "$files" | xargs perl -pi -w -e 's/\s(http[^\s]+)\b/ [$1]($1)/g;'
